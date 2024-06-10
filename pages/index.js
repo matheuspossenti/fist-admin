c760235a3c352cc3eda35e003f97be8c9fce1247
Matheus Possenti <matheus.possenti24@gmail.com>
2024-06-10 08:29:37 -0300

    Admin Basic Design Feature

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,19 @@
-import Nav from "@/components/Nav";
-import { useSession, signIn, signOut } from "next-auth/react"
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const { data: session } = useSession()
-  if(!session){
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={() => (signIn('google'))} className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+  return <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+          <img src={session?.user?.image} alt={session?.user?.name} className="w-6 h-6" />
+          <span className="px-2">
+            {session?.user?.name}
+          </span>
         </div>
-    </div>
-    );
-  }
-    return (
-      <div className="bg-blue-900 min-h-screen flex">
-        <Nav />
-        <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">Logged in with {session.user.email}!</div>
       </div>
-    )
+    </Layout>
 }
