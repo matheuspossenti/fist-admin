badfc57fad30ffd4a2b641d1abbc462b0a90ab0f
Matheus Possenti <matheus.possenti24@gmail.com>
2024-06-12 11:48:14 -0300

    :sparkles: feat: adding product categories

---
+++ pages/api/categories.js
@@ -0,0 +1,18 @@
+import { mongooseConnect } from "@/lib/mongoose";
+import { Category } from "@/models/Category";
+
+export default async function handle(req, res){
+  const {method} = req;
+  await mongooseConnect();
+
+  if (method === 'GET'){
+    const categories = await Category.find();
+    res.json(categories);
+  }
+
+  if(method === 'POST'){
+    const {name} = req.body;
+    const categoryDoc = await Category.create({name});
+    res.json(categoryDoc);
+  }
+}
