00fa17e2411fb015de324a0b2f7f92a48c746163
Matheus Possenti <matheus.possenti24@gmail.com>
2024-06-27 22:30:21 -0300

    admin teste

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,7 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth, { getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const adminEmails = ["matheus.possenti24@gmail.com"];
+const adminEmails = ["matheus.possenti24@gmail.com", "vslongo@gmail.com"];
 
 export const authOptions = {
   providers: [
